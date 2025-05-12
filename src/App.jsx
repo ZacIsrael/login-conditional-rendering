8309a0154e0dd104432d7993bce6ad40aa0fc6a4
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-12 10:34:09 -0400

    Created a separate component for the login form.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  // Each component should have a single responsibiltiy
+
+  // boolean that indicates whether or not a user is logged in
+  let isLoggedIn = false;
 
   return (
     <>
       <div className="container">
         <h1>Hello</h1>
-        <form className="form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
+        <Form />
       </div>
     </>
   );
