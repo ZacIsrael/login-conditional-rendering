5c9da77dedca0b6b7fe230f11303b44289b73951
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-12 11:33:41 -0400

    Simplified the ternary operators in the Form component.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,11 +4,12 @@ import Input from "./Input";
 function Login(props) {
   return (
     <form className="form">
-      {props.registered ? 
-      <><Input type="text" placeholder="Username" /><Input type="text" placeholder="Password" /><button type="submit">Login</button></>
-   : 
-   <><Input type="text" placeholder="Username" /><Input type="text" placeholder="Password" /><Input type="text" placeholder="Confirm Password" /><button type="submit">Register</button></>
-   }
+      <Input type="text" placeholder="Username" />
+      <Input type="text" placeholder="Password" />
+      {/* only display the "Confirm Password" input field if the user has not been registered */}
+      {!props.registered && <Input type="text" placeholder="Confirm Password" />}
+      {props.registered ? <button type="submit">Login</button> : <button type="submit">Register</button>}
+    
     </form>
   );
 }
