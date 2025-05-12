9119ce4eae58b9d56910294158175902471875f8
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-12 11:05:15 -0400

    Added more notes and examples of ternary operators.

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,13 @@ function App() {
         {/* if it is at least 6:00 pm (17 is 5:00 pm but I'm not calculating minutes), 
         ask the user why they are still working; otherwise, render the Login component */}
         {new Date().getHours() > 17 ? <h1>Why are you still working?</h1> : <Login />}
+
+        {/* The following lines of code do the exact same thing */}
+        {new Date().getHours() > 17 ? <h1>Why are you still working?</h1> : null}
+        {/* && allows javascript to read the condition on the left; if it's false,
+        javascript won't even waste its time trying to evaluate the function on 
+        the right because it does not matter. && needs both statements to be true */}
+        {new Date().getHours() > 17 && <h1>Why are you still working?</h1>}
       </div>
     </>
   );
